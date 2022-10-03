b364082c3420b3f22dd452e0700beb76e40b3845
ClaudiaAriasH <claudiaarias151574@correo.itm.edu.co>
2022-10-02 19:48:21 -0500

    Filtros Gateway Factory que vienen de fábrica

--- Curso-Spring-Udemy/items-api/src/main/java/com/curso/items/controllers/ItemController.java
+++ Curso-Spring-Udemy/items-api/src/main/java/com/curso/items/controllers/ItemController.java
@@ -2,11 +2,15 @@ package com.curso.items.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.items.models.Item;
@@ -18,13 +22,18 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @CrossOrigin("*")
 public class ItemController {
 	
+	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+	
+	
 	@Autowired
 	@Qualifier("itemServiceFeign")
 	//@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
-	public List<Item> listar() {
+	public List<Item> listar(@RequestParam(name="nombre") String nombre, @RequestHeader(name="token-request") String token) {
+		logger.info("RequestParameter: " + nombre);
+		logger.info("RequestHeader: " + token);
 		return itemService.findAll();		
 	}
 
